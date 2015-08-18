5ae9d8e20991a45211a5d8769f7b19116226195d
Joseph A Mark <sjeohp@gmail.com>
2015-08-18 14:40:17 +1200

    minor tidyup

--- collisions.h
+++ collisions.h
@@ -17,74 +17,72 @@ class Collidable
 {
 private:
 	int type_;
-	
-    bool isCircle_;
-    float restitution_;
-    float inertia_;
-	
+
+	bool isCircle_;
+	float restitution_;
+	float inertia_;
+
 	/// CIRCLE
 	float centrex_;
 	float centrey_;
-    uint radius_;
-	
+	uint radius_;
+
 	/// POLYGON
-    uint nvert_;
-    float* vertx_;
-    float* verty_;
-    float* normx_;
-    float* normy_;
+	uint nvert_;
+	float* vertx_;
+	float* verty_;
+	float* normx_;
+	float* normy_;
 	
 public:
 	
 	int id;
-	
-    Collidable() : isCircle_(false) {}
-    Collidable(float x, float y, uint radius) : isCircle_(true), centrex_(x), centrey_(y), radius_(radius) {}
-    Collidable(uint nvert, float* vertx, float* verty);
+
+	Collidable() : isCircle_(false) {}
+	Collidable(float x, float y, uint radius) : isCircle_(true), centrex_(x), centrey_(y), radius_(radius) {}
+	Collidable(uint nvert, float* vertx, float* verty);
 	Collidable(float x, float y, float w, float h);
-    void setVertices(uint nvert, float* vertx, float* verty);
+	void setVertices(uint nvert, float* vertx, float* verty);
 	void setNormals(uint nvert, float* vertx, float* verty, float* normx, float* normy);
-    void transformVertices(glm::vec2 position, float angle);
+	void transformVertices(glm::vec2 position, float angle);
 	int type() const { return type_; }
 	void setType(int type) { type_ = type; }
 	
 public:
-    bool isCircle() const { return isCircle_; }
+	bool isCircle() const { return isCircle_; }
 	float centreX() const { return centrex_; }
 	float centreY() const { return centrey_; }
-    uint radius() const { return radius_; }
-    uint nvert() const { return nvert_; }
-    float* vertx() const { return vertx_; }
-    float* verty() const { return verty_; }
-    float* normx() const { return normx_; }
-    float* normy() const { return normy_; }
-    float restitution() const { return restitution_; }
-    float inertia() const { return inertia_; }
+	uint radius() const { return radius_; }
+	uint nvert() const { return nvert_; }
+	float* vertx() const { return vertx_; }
+	float* verty() const { return verty_; }
+	float* normx() const { return normx_; }
+	float* normy() const { return normy_; }
+	float restitution() const { return restitution_; }
+	float inertia() const { return inertia_; }
 };
-
-glm::vec2 supportPoint(Collidable* item, glm::vec2 dir);
-float penetration(Collidable* item_a, Collidable* item_b, uint* face_index, glm::vec2* support_point);
-void setResolve(void(*resolve)(Collidable*, Collidable*, int, glm::vec2));
 
 class Quadtree
 {
 private:
-    std::vector<Quadtree> quads_;
-    float* vertx_;
-    float* verty_;
-    std::vector<Collidable*> items_;
+	std::vector<Quadtree> quads_;
+	float* vertx_;
+	float* verty_;
+	std::vector<Collidable*> items_;
 public:
-    Quadtree() {}
-    Quadtree(float* vertx, float* verty, uint depth);
-    float* vertx() const;
-    float* verty() const;
-    std::vector<Collidable*> items() const;
-    std::vector<Quadtree> quads() const;
+	Quadtree() {}
+	Quadtree(float* vertx, float* verty, uint depth);
+	float* vertx() const;
+	float* verty() const;
+	std::vector<Collidable*> items() const;
+	std::vector<Quadtree> quads() const;
 	void clear();
-    void insert(Collidable* item);
+	void insert(Collidable* item);
 	void possibleCollision(Collidable* item_a, Collidable* item_b) const;
-    std::vector<Collidable*> checkCollisions() const;
+	std::vector<Collidable*> checkCollisions() const;
 };
+
+void setResolve(void(*resolve)(Collidable*, Collidable*, int, glm::vec2));
 
 }
 
